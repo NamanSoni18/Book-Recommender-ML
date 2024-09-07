529189af32205763475bfc5faa9cc92d208b845d
Naman Soni <sknaman763@gmail.com>
2024-09-07 08:31:02 +0530

    Updated

--- frontend/Book-Recommendation-System/components/gradual-spacing.tsx
+++ frontend/Book-Recommendation-System/components/gradual-spacing.tsx
@@ -4,7 +4,7 @@ export async function GradualSpacingDemo() {
   return (
     <GradualSpacing
       className="font-display text-center text-2xl font-bold tracking-[-0.1em]  text-black dark:text-white"
-      text="Book Recommendation System"
+      text="Book Recommender"
     />
   );
 }
